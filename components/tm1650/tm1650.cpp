5aa89f4da93181ef7f4f719bca7e9a4a8585655f
Daniel Wyschka <daniel.wyschka@staudacher.de>
2023-10-13 19:07:31 +0200

    tbd

--- components/tm1650/tm1650.cpp
+++ components/tm1650/tm1650.cpp
@@ -88,9 +88,9 @@ void TM1650Display::display() {
 */
 
   this->send_byte_(TM1650_DATA_WR_CMD | 1 << 1);						// address command + address (68,6A,6C,6E)
-  this->send_byte_(0b10111111);
+  this->send_byte_(0b11101111);
   this->send_byte_(TM1650_DATA_WR_CMD | 2 << 1);						// address command + address (68,6A,6C,6E)
-  this->send_byte_(0b11011111);
+  this->send_byte_(0b11110111);
   this->stop_();
 }
 
