9ddcbbccb9b76f9a59f1352e86a84042aaed1acc
Daniel Wyschka <daniel.wyschka@staudacher.de>
2023-10-14 12:21:27 +0200

    tbd

--- components/tm1650/tm1650.cpp
+++ components/tm1650/tm1650.cpp
@@ -155,7 +155,6 @@ uint8_t TM1650Display::get_keys() {
   this->stop_();
 
   if ((key_code & TM1650_SW_MSK) == TM1650_SW_MSK) {
-        ESP_LOGD("KEYS", "Got Key %d", key_code);
         return key_code;
   }
 
